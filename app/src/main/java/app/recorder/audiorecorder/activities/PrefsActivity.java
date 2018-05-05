f519442ae33990132a231424dd4ab5c740e6d90e
anrosant <anrosant@espol.edu.ec>
2018-05-04 23:08:12 -0500

    Presentar en el menú el valor actual

--- app/src/main/java/app/recorder/audiorecorder/activities/PrefsActivity.java
+++ app/src/main/java/app/recorder/audiorecorder/activities/PrefsActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceGroup;
 import android.util.Log;
 import app.recorder.audiorecorder.R;
 import app.recorder.audiorecorder.services.AudioRecorderService;
@@ -23,9 +25,28 @@ public class PrefsActivity extends PreferenceActivity implements SharedPreferenc
         addPreferencesFromResource(R.xml.prefs);
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); ++i) {
+            Preference preference = getPreferenceScreen().getPreference(i);
+            if (preference instanceof PreferenceGroup) {
+                PreferenceGroup preferenceGroup = (PreferenceGroup) preference;
+                for (int j = 0; j < preferenceGroup.getPreferenceCount(); ++j) {
+                    Preference singlePref = preferenceGroup.getPreference(j);
+                    Log.d("pref", String.valueOf(singlePref));
+                    updatePreference(singlePref, singlePref.getKey());
+
+                }
+            } else {
+                updatePreference(preference, preference.getKey());
+            }
+        }
+    }
 
     //LISTENER QUE DETECTA CAMBIOS EN LAS PREFERENCIAS
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updatePreference(findPreference(key), key);
         Log.d("METODO CHANGE", "CAMBIÓ LA CONFIGURACIÓN: " + key);
         if (key.equals("audio_duration") || key.equals("recording_audio_interval")) {
             Log.d("METODO CHANGE", "ENTRÓ");
@@ -35,6 +56,28 @@ public class PrefsActivity extends PreferenceActivity implements SharedPreferenc
                     recordingAudioInterval + audioDuration , pendingIntent);
             Log.d("ALARMA", "ALARMA CAMBIADA");
         }
+    }
+    private void updatePreference(Preference preference, String key) {
+        if (preference == null) return;
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            listPreference.setSummary(listPreference.getEntry());
+            return;
+        }
+        SharedPreferences sharedPrefs = getPreferenceManager().getSharedPreferences();
+        if(key.equals("recording_audio_interval")){
+            preference.setSummary(sharedPrefs.getString(key, "Default")+" Segundos");
+        }else if(key.equals("audio_duration")){
+            preference.setSummary(sharedPrefs.getString(key, "Default")+ " Segundos");
+        }else if(key.equals("audio_encode_bitrate")){
+            preference.setSummary(sharedPrefs.getString(key, "Default")+" Kbps \n" +
+                    "Nota: 16 kbps a 256 kbps (mono) / 16 a 448 kbps (stereo)");
+        }
+        else{
+            preference.setSummary(sharedPrefs.getString(key, "Default"));
+        }
+
+
     }
 
 }
